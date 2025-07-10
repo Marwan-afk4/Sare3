2bb0a3ed68b28fdf21e92670af4f6156f9b560fe
Marwan Gaber <fk413691@gmail.com>
2025-07-11 02:59:09 +0300

    update fireabse json

--- app/Http/Controllers/Api/User/RideEstimateController.php
+++ app/Http/Controllers/Api/User/RideEstimateController.php
@@ -105,7 +105,7 @@ class RideEstimateController extends Controller
 
         try{
             $firebase = (new Factory)
-                ->withServiceAccount(storage_path('firebase/sarea-adce3-firebase-adminsdk-fbsvc-c1bd05e41d.json'))
+                ->withServiceAccount(storage_path('firebase/sarea-adce3-firebase-adminsdk-fbsvc-892a07f354.json'))
                 ->withDatabaseUri('https://sarea-adce3-default-rtdb.firebaseio.com')
                 ->createDatabase();
 
