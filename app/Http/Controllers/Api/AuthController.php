e52cab3f45cf8650066a910f2e7d979944fc3f50
Marwan Gaber <fk413691@gmail.com>
2025-07-11 23:26:42 +0300

    delete message phone alredy been token

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -133,7 +133,7 @@ class AuthController extends Controller
             if ($phoneUsedByAnother) {
                 $token = $user->createToken('auth_token')->plainTextToken;
                 return response()->json([
-                    'message' => 'Phone number already used by another account',
+                    // 'message' => 'Phone number already used by another account',
                     'token'=> $token
                 ], 409);
             }
