15318f71e0531edcf769c09d9412e253bf6fa8f1
Marwan Gaber <fk413691@gmail.com>
2025-07-27 04:08:11 +0300

    check null

--- app/Http/Controllers/Api/User/ProfileController.php
+++ app/Http/Controllers/Api/User/ProfileController.php
@@ -25,7 +25,7 @@ class ProfileController extends Controller
         // Map ride data
         $ridesData = $completedRides->map(function ($ride) {
             $driver = optional($ride->driver);
-            $car = optional($driver->driverCars->first());
+            $car = optional($driver->driverCars->first()??null);
 
             return [
                 'ride_id' => $ride->id,
