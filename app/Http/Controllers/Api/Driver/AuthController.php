adf752992dfae96d44922d2bb5cdcc7c6676d3ce
Marwan Gaber <fk413691@gmail.com>
2025-08-11 00:45:04 +0300

    fcmtpoken add in name api in auth in driver

--- app/Http/Controllers/Api/Driver/AuthController.php
+++ app/Http/Controllers/Api/Driver/AuthController.php
@@ -147,7 +147,8 @@ class AuthController extends Controller
         $validation = Validator::make($request->all(), [
             'phone' => 'required|string|exists:users,phone',
             'name' => 'required|string|max:255',
-            'gender' => 'required|string|in:male,female'
+            'gender' => 'required|string|in:male,female',
+            'fcm_token' => 'required|string'
         ]);
         if ($validation->fails()) {
             return response()->json([
@@ -160,6 +161,7 @@ class AuthController extends Controller
             $user->role = 'driver';
             $user->activity = 'in_progress';
             $user->gender = $request->gender;
+            $user->fcm_token = $request->fcm_token;
             $user->wallet = 0; // Initialize wallet to 0
             $user->save();
             return response()->json([
