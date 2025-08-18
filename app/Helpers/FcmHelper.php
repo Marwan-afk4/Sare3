a81ade3155ccbd164976750e41065a5f6ab87ebb
Marwan Gaber <fk413691@gmail.com>
2025-08-19 01:16:05 +0300

    project_id for firebase helper

--- app/Helpers/FcmHelper.php
+++ app/Helpers/FcmHelper.php
@@ -38,7 +38,7 @@ class FcmHelper
 
         $response = Http::withToken($accessToken)
         ->withoutVerifying() // Disable SSL verification (not secure for production)
-        ->post("https://fcm.googleapis.com/v1/projects/quran-chat-ae0c1/messages:send", $payload);
+        ->post("https://fcm.googleapis.com/v1/projects/sarea-adce3/messages:send", $payload);
 
         return $response->json();
     }
