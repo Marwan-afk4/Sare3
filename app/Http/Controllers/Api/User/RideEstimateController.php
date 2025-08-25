c1b447a77e2fc34ddf1ffcfadc08f90aa2f34500
Marwan Gaber <fk413691@gmail.com>
2025-08-25 21:20:04 +0300

    60

--- app/Http/Controllers/Api/User/RideEstimateController.php
+++ app/Http/Controllers/Api/User/RideEstimateController.php
@@ -459,7 +459,7 @@ class RideEstimateController extends Controller
             }
 
             // بعد ما تحدث ride بالـ nearest driver
-            $timeLimit = RideRequestTimeLimit::first()->time_limit_seconds ?? 5;
+            $timeLimit = RideRequestTimeLimit::first()->time_limit_seconds ?? 60;
 
             dispatch(new HandleDriverTimeout($ride->id, $nearestDriver['id']))
                 ->delay(now()->addSeconds($timeLimit));
