4985029d4571d75e3ac14eeaa5d8be5e800448fd
Marwan Gaber <fk413691@gmail.com>
2025-08-29 04:56:54 +0300

    test

--- app/Observers/RideObserver.php
+++ app/Observers/RideObserver.php
@@ -23,21 +23,21 @@ class RideObserver
         // Check if status was changed
         if ($ride->isDirty('status')) {
             $newStatus = $ride->status->value;
-            $oldStatus = $ride->getOriginal('status');
+            $oldStatus = $ride->getOriginal('status')->value;
 
             Log::info("Ride {$ride->id} status changed from {$oldStatus} to {$newStatus}");
 
             // Update Firebase with new status
             $this->firebaseService->updateRideStatus(
-                $ride->id, 
-                $newStatus, 
+                $ride->id,
+                $newStatus,
                 $ride->firebase_ride_id
             );
 
             // If ride is completed, cleanup Firebase tracking data
             if (in_array($newStatus, ['completed', 'finished', 'finshed'])) {
                 Log::info("Ride {$ride->id} completed, cleaning up Firebase tracking data");
-                
+
                 // Wait a moment before cleanup to ensure status update is received
                 dispatch(function () use ($ride) {
                     sleep(2); // Wait 2 seconds
