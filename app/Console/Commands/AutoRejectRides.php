0dc0fd7badc6f414d6f50f4a97ead3c6301c531a
Marwan Gaber <fk413691@gmail.com>
2025-10-31 00:11:18 +0200

    yarb b2a

--- app/Console/Commands/AutoRejectRides.php
+++ app/Console/Commands/AutoRejectRides.php
@@ -20,7 +20,7 @@ class AutoRejectRides extends Command
     public function handle()
     {
         Log::info('🚀 Auto reject command started at ' . now());
-        $expiredTime = Carbon::now()->subSeconds(15);
+        $expiredTime = Carbon::now()->subSeconds(30);
 
         $rides = Ride::where('status', 'pending')
             ->whereNotNull('driver_assigned_at')
