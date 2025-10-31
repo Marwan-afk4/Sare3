3e1f460d8b3aa532724ed7bdbed396102290b41d
Marwan Gaber <fk413691@gmail.com>
2025-10-31 02:02:31 +0200

    dome

--- app/Http/Controllers/Api/User/RideEstimateController.php
+++ app/Http/Controllers/Api/User/RideEstimateController.php
@@ -388,9 +388,9 @@ class RideEstimateController extends Controller
             foreach ($eligibleDrivers as $i => &$driver) {
                 $elements = $rows[$i]['elements'] ?? [];
                 if (!empty($elements) && ($elements[0]['status'] ?? '') === 'OK') {
-                    $driver['eta_time'] = $elements[0]['duration']['value']; // seconds
+                    $driver['eta_time'] = $elements[0]['duration']['value'];
                 } else {
-                    $driver['eta_time'] = null;
+                    $driver['eta_time'] = 9999999;
                 }
             }
 
